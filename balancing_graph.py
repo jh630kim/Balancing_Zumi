9d7e7e6a1f66acbf8d988c48ef7faa8171e79bd3
100-Jung <jh630.kim@gmail.com>
2023-01-29 22:05:27 +0900

    1차 완성본(LPF 이용)
    LPF를 이용한 1차 완성본이다.
    다만, Set Point와 PID 계수를 제대로 잡지 못했다.
    그런건지... 센서 데이터가 엉망이어서 그런건지는 모르겠다.
    센서의 필터를 바꿔서 측정 해 보자.
    센서 데이터는 blog에 임시저장 해 두었다.
    모터를 동작시키면, Zumi가 떨리면 센서값의 편차가 커진다.

--- balancing_graph.py
+++ balancing_graph.py
@@ -14,6 +14,8 @@ import time
 import sys
 import datetime
 
+import _41_FILE_record as rec
+
 from zumi.zumi import Zumi
 from simple_pid import PID
 
@@ -79,7 +81,7 @@ setpoint_adj = 0
 # todo: Sampling Time 조절, zippy 코드를 보니 5ms 이내로 하라고 한다.
 #       가장 마지막의 time.sleep()도 조정 필요
 sample_time = 0.01  # 주기: 0.05 sec
-output_limits = (-126, 127)   # 출력 제한: -126 ~ 127
+output_limits = (-99, 99)   # 출력 제한: -100 ~ 100
 auto_mode = True    # PID control ON(True), Off(False)
 p_ON_M = False      # 뭐지?
 
@@ -100,8 +102,8 @@ pid = PID(Kp=Default_Kp, Ki=Default_Ki, Kd=Default_Kd,
 dt = datetime.datetime.now()
 dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second, dt.microsecond
 
-# file_name = './run_setting_log.txt'
-file=open('run_setting_log.txt', 'a')
+file_name = './run_setting_log.txt'
+file=open(file_name, 'a')
 file.write('{}/{} {}:{} {} {} {} {} {}\n'.format(
     dt.month, dt.day, dt.hour, dt.minute, Default_SP, Default_Kp, Default_Ki, Default_Kd, sample_time))
 file.close()
@@ -121,18 +123,20 @@ file.close()
 ########################################
 # Logging 준비
 ########################################
+# 전송 센서 정보
 if logging == 1:
-    # 파일명
-    file_name = './sensor_' + str(int(time.time())) + '.csv'
-    # Data 메시지 준비
-    data = []  # 100개 데이터 스트링를 저장할 빈 list
-    title = "acc_xf, "\
-            "motor_speed, "\
-            "elapsed_time"
+    sensor_name = ["acc_xf", ",",
+                   "motor_speed", ",",
+                   "elapsed_time"]
 
-    file = open(file_name, 'w')
-    file.write(title + '\n')
-    file.close()
+    # Data 메시지 준비
+    data = []
+    value = ''
+    for i in range(len(sensor_name)):
+        value += '{} '
+
+    # Clinet 초기화
+    rec.initialize(sensor_name)
 
 ########################################
 # 제어 루프 시작
@@ -164,6 +168,131 @@ gyro_pitch_cal = gyro_pitch_sum/500
 gyro_yaw_cal = gyro_yaw_sum/500
 '''
 
+############################################################
+#-----------------------------------
+# 실시간 그래프 그리기 (센서값 and 모터속도)
+############################################################
+
+alpha = 0.3
+prev_acc_xf = zumi.update_angles()[8]    # rot_y를 이용
+time.sleep(0.01)
+
+motor_speed = 0     # todo: 이건 뭐지?
+count = 0           # display 주기
+elapsed_time100 = 0
+past = time.time()  # 루프 주기 체크용
+
+# 초기값 선언
+MAX_X = 100
+lst = [np.zeros(MAX_X),
+       np.zeros(MAX_X)]          # 100개의 Zero 값
+t = np.linspace(0,1,MAX_X)   # 0~1까지 100단계로 나눔
+
+# step 1.
+# fig = 그래프의 설정(plt.figure) 관련 정보
+# 다수의 그래프를 그림
+# fig는 전체, axes는 각각의 화면을 의미
+fig, axes1 = plt.subplots(nrows=1, ncols=1)
+axes2 = axes1.twinx()   # 첫번째 plot의 Twin
+
+# 초기 그래프 그리기
+# line 객체의 첫번째... ln = plt.plot(t,lst,'r')[0]과 같은 의미임
+# ln, = plt.plot(t,list,'r')과 같은 의미임
+ln = [axes1.plot(t, lst[0], 'r')[0],
+      axes2.plot(t, lst[1], 'g')[0]]
+
+# 그리드 그리기
+axes1.grid(True)
+axes2.grid(True)
+
+# 그래프의 최대 최소값
+# plt.xlim(0,100)   # x값은 t로 정해진다.
+# subplots의 axes의 경우 ylim 대신 set_ylim을 사용
+def change_ylim(axes1, axes2, lst):
+    axes1.set_ylim(min(lst[0]), max(lst[0]))
+    axes2.set_ylim(min(lst[1]), max(lst[1]))
+
+axes1.set_ylim(-5, 5)
+axes2.set_ylim(-5, 5)
+
+
+def update(index):
+    global prev_acc_xf, count, elapsed_time100, past
+    global data, value
+
+    #################################
+    # 센서 읽기 + 1차 LPF 적용
+    #################################
+    # raw_data = zumi.get_all_mpu_data()[0] * multi  # acc_x를 이용한 LPF 적용
+    raw_data = zumi.update_angles()[8]  # rot_y를 이용
+    acc_xf = alpha * prev_acc_xf + (1 - alpha) * raw_data
+    prev_acc_xf = acc_xf
+
+    # 현재 각도
+    current_angle = acc_xf
+
+    ##################
+    # PID 제어
+    ##################
+    # current_angle = 현재 Y 각도(pitch)
+    # motor_speed: 모터의 전/후진
+    motor_speed = pid(current_angle)
+
+    ###############
+    # Data Logging
+    ###############
+    count += 1
+    new = time.time()
+    elapsed_time = new - past
+    elapsed_time100 += elapsed_time
+    past = new
+
+    if logging == 1:
+        # 메시지 생성 for logging
+        data.append(value.format(acc_xf, ",",
+                                 motor_speed, ",",
+                                 elapsed_time))
+        '''print(f"{acc_xf}, "
+              f"{motor_speed}, "
+              f"{elapsed_time}, ")'''
+        # 그래프를 그리기 위해 전송(또는 Logging)
+        print("acc_xf: {:10.6f}, speed: {:10.6f}".format(acc_xf, motor_speed))
+
+        if count > 100:
+            rec.msg_send(data)
+            # 메시지 전송 후 초기화
+            data = []
+
+    lst[0] = np.append(lst[0], acc_xf)
+    lst[0] = np.delete(lst[0], 0)
+    lst[1] = np.append(lst[1], motor_speed)
+    lst[1] = np.delete(lst[1], 0)
+
+    # Line Update
+    ln[0].set_data(t, lst[0])
+    ln[1].set_data(t, lst[1])
+
+    if count > 100:
+        print("Elapsed time(for 100 tries) =", elapsed_time100)
+        count = 0
+        elapsed_time100 = 0
+
+    change_ylim(axes1, axes2, lst)
+    # 참고로, blit가 True인 경우만 Return값이 필요하다.
+    return ln[0], ln[1]
+
+
+# step 3. frame = 2번의 func 함수가 실행될 때 마다 2번의 func 함수의 argument로서 전달할 값의 list
+ani = FuncAnimation(fig=fig, func=update, frames=t, blit=True)
+
+plt.show()
+
+############################################################
+# 실시간 그래프 그리기 (센서값 and 모터속도)
+#-----------------------------------
+############################################################
+
+'''
 ##################################
 # 초기값 설정
 ##################################
@@ -197,7 +326,6 @@ try:
         # motor_speed: 모터의 전/후진
         motor_speed = pid(current_angle)
 
-        '''
         # ----불필요???--------------------
         # 정지상태의 Setpoint 조정
         adj_value = 0.01      # 0.015
@@ -207,15 +335,9 @@ try:
             setpoint_adj += adj_value
         # pid.setpoint = setpoint + setpoint_adj
         # ------------------------
-        '''
 
-        motor_speed = -motor_speed
-        time1 = time.time()
         if run == 1:
-            zumi.control_motors(left=round(motor_speed),
-                                right=round(motor_speed),
-                                acceleration=0)   # 이 명령만 10ms를 잡아먹는다.
-        time2 = time.time()
+            zumi.control_motors(left=motor_speed, right=motor_speed, acceleration=0)
 
         ###############
         # Data Logging
@@ -226,23 +348,18 @@ try:
         elapsed_time100 += elapsed_time
         past = new
 
-        if count%10 == 0:
-            print("acc_xf: {:10.6f}, speed: {:10.6f}, time: {:10.6f}"
-                  .format(acc_xf, motor_speed, time2-time1))
-
         if logging == 1:
             # 메시지 생성 for logging
-            data.append("{}, {}, {}".format(acc_xf,
-                                            motor_speed,
-                                            elapsed_time))
-
+            data.append(value.format(acc_xf, ",",
+                                     motor_speed, ",",
+                                     elapsed_time))
             # 그래프를 그리기 위해 전송(또는 Logging)
+             print(f"acc_xf: {acc_xf:10.6f}, "
+                   f"speed: {motor_speed:10.6f}")
+                   
             if count > 100:
-                file = open(file_name, 'a')
-                for str in data:
-                    file.write(str + '\n')
-                file.close()
-                # 메시지 전송 후 데이터 리스트 초기화
+                rec.msg_send(data)
+                # 메시지 전송 후 초기화
                 data = []
 
         if count > 100:
@@ -260,11 +377,9 @@ finally:
     # always have a stop at the end of code with control motors
     print('Stop Zumi')
     zumi.stop()
+'''
 
-    # 남은 데이터 로깅
-    file = open(file_name, 'a')
-    for str in data:
-        file.write(str + '\n')
-    file.close()
+# 소켓 닫기
+rec.close()
 
 
